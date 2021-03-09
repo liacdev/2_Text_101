8e879f81386fd8b0a116fb059a8d71b6a5f796b2
Jago <jagoblacksky@gmail.com>
2021-03-09 01:39:27 -0600

    5. Update text component
    Added AdventureGame script

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = ("Get ready to go to work!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

b6e0d81a74449dd78c91679a1334cde7f5ab2110
Jago <jagoblacksky@gmail.com>
2021-03-10 12:53:03 -0600

    Lessons 11-12
    Added states and corrected framework issues

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -9,8 +9,6 @@ public class AdventureGame : MonoBehaviour
     [SerializeField] Text textComponent;
     [SerializeField] State startingState;
 
-    
-
     State state;
     
     // Start is called before the first frame update
@@ -23,8 +21,35 @@ public class AdventureGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
     }
 
+    private void ManageState()
+    {
+        var nextState = state.GetNextState();
+
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextState[0];
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextState[1];
+        }
+     
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextState[2];
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            state = nextState[3];
+        }
+
+        textComponent.text = state.GetStateStory();
+
+    }
  
 }
